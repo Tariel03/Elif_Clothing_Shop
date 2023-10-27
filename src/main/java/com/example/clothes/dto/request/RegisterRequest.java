2b826ca2bd5184a318046a15564816d457c2b8b4
MusaevaAdelya <msvadelya@gmail.com>
2023-10-27 23:23:28 +0600

    added security configuration

--- src/main/java/com/example/clothes/dto/request/RegisterRequest.java
+++ src/main/java/com/example/clothes/dto/request/RegisterRequest.java
@@ -3,7 +3,7 @@ package com.example.clothes.dto.request;
 import lombok.Data;
 
 @Data
-public class RegisterDTO {
+public class RegisterRequest {
 
 	private String firstName;
 	private String lastName;
